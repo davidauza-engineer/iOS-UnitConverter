fbdb41bc74de34cbea794d6c868be31ca937286f
davidauza.engineer <davidauza01@gmail.com>
2019-10-21 07:08:15 -0500

    Add base logic

--- UnitConverter/ViewController.m
+++ UnitConverter/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 
+// TODO use Objective-C methods and a Switch structure for updateButton method
+// Optionally: Add reset button.
+
 double convertUnitOneToUnitTwo(double unitOneValue) {
     double unitTwoValue;
     unitTwoValue = (10 * unitOneValue) + 2;
@@ -39,6 +42,8 @@ double convertUnitOneToUnitTwo(double unitOneValue) {
         [buf appendString:@"unit four"];
     }
     self.outputField.text = buf;
+    // Hide keyboard
+    [self.view endEditing:YES];
 }
 
 - (void)viewDidLoad {
